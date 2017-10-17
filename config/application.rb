e18d64775838f8e2c10c92ee8c7f9faace70b80f
tdcom <tony@tdcom.co.uk>
2017-10-17 08:42:00 +0000

    witutf encoding

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.encoding = "utf-8"
 module Dnsreader
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
